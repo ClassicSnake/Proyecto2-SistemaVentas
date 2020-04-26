f9da3f440212b3b9d333df807f1c46b9d8186c7e
unknown <george.vid29@gmail.com>
2020-04-26 13:15:02 -0700

    primer commit

---
+++ src/Entidades/Provider.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author George
+ */
+@Entity
+@Table(name = "provider")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Provider.findAll", query = "SELECT p FROM Provider p")
+    , @NamedQuery(name = "Provider.findByIdProvider", query = "SELECT p FROM Provider p WHERE p.idProvider = :idProvider")
+    , @NamedQuery(name = "Provider.findByName", query = "SELECT p FROM Provider p WHERE p.name = :name")
+    , @NamedQuery(name = "Provider.findByAddress", query = "SELECT p FROM Provider p WHERE p.address = :address")
+    , @NamedQuery(name = "Provider.findByPhone", query = "SELECT p FROM Provider p WHERE p.phone = :phone")
+    , @NamedQuery(name = "Provider.findByWebsite", query = "SELECT p FROM Provider p WHERE p.website = :website")})
+public class Provider implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "idProvider")
+    private Integer idProvider;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "website")
+    private String website;
+
+    public Provider() {
+    }
+
+    public Provider(Integer idProvider) {
+        this.idProvider = idProvider;
+    }
+
+    public Integer getIdProvider() {
+        return idProvider;
+    }
+
+    public void setIdProvider(Integer idProvider) {
+        this.idProvider = idProvider;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idProvider != null ? idProvider.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Provider)) {
+            return false;
+        }
+        Provider other = (Provider) object;
+        if ((this.idProvider == null && other.idProvider != null) || (this.idProvider != null && !this.idProvider.equals(other.idProvider))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entidades.Provider[ idProvider=" + idProvider + " ]";
+    }
+    
+}
